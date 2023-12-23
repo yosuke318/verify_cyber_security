b499f6cb1a0ac37e3652829cd5a490bee6b3380e
Yosuke Santamaria <youman.318@gmail.com>
2023-12-23 20:33:42 +0900

    edit get method

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,15 @@ const express = require("express");
 const router = express.Router();  // ルーティング用オブジェクト
 
 router.get("/", (req, res) => {
-  res.send({ message: "Hello World" });
+    let message = req.query.message;
+    res.send({ message });
+});
+
+router.use(express.json());
+router.post("/", (req, res)=>{
+    const body = req.body;  // リクエストボディを受け取る設定
+    console.log(body);
+    res.end();
 });
 
 module.exports = router;  // 他のファイルから読み取れるようにする
