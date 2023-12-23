deb3e86bdf0698c14681cbafe6a738428773610f
Yosuke Santamaria <youman.318@gmail.com>
2023-12-23 10:34:22 +0900

    server.js

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express=require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res, next)=>{
+app.use(express.static("public"))
 
-})
+app.get("/", (req, res, next)=>{
+    res.end("Top Page");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
